925b4d01c448d22b65fe9fa341d70f337428a335
gabrielrsp <gabrielrsp18@gmail.com>
2020-07-04 20:38:24 -0300

    adjust repository layout

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi'
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -75,7 +76,7 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map( repository => (
-                <a key={repository.full_name} href="test">
+                <Link key={repository.full_name} to={`/repositories/${repository.full_name}`}>
                 <img
                   src={repository.owner.avatar_url}
                   alt={repository.owner.login} />
@@ -84,7 +85,7 @@ const Dashboard: React.FC = () => {
                   <p>{repository.description}</p>
                 </div>
                 <FiChevronRight size={20} />
-              </a>
+              </Link>
 
         ) )}
       </Repositories>
--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,139 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+
+interface RepositoryParams {
+  repository: string;
+}
+
+interface Repository {
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  forks_count: number;
+  open_issues_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
+
+interface Issue {
+  id: number;
+  title: string;
+  html_url: string;
+  user: {
+    login: string;
+  }
+}
+
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>
+
+  const [repository, setRepository] = useState<Repository | null>(null);
+  const [issues, setIssues] = useState<Issue[]>([]);
+
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  useEffect(() => {
+
+
+    api.get(`repos/${params.repository}`).then((response) => {
+      setRepository(response.data);
+    });
+
+    api.get(`repos/${params.repository}/issues`).then((response) => {
+      setIssues(response.data);
+    })
+
+
+    /*
+      Fetching from API USING ASYNC AWAIT
+
+      On the method below, the requests aren't executed at the same time,
+      a second request is only executed when the previous is done
+      So I have to use the method " await Promise.all() " to do many requests at same time
+    */
+
+    /*
+    async function loadData(): Promise<void> {
+
+      const [repository, issues] = await Promise.all([
+        api.get(`repos/${params.repository}`),
+        api.get(`repos/${params.repository}/issues`),
+      ]);
+
+    }
+
+    loadData();
+    */
+
+
+  }, [params.repository]);
+
+
+
+
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+      </Link>
+      </Header>
+
+      {repository ? (
+
+        <RepositoryInfo>
+          <header>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login} />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+          </header>
+          <ul>
+            <li>
+              <strong>{repository.stargazers_count}</strong>
+              <span>Stars</span>
+            </li>
+            <li>
+              <strong>{repository.forks_count}</strong>
+              <span>Forks</span>
+            </li>
+            <li>
+              <strong>{repository.open_issues_count}</strong>
+              <span>Open Issues</span>
+            </li>
+          </ul>
+        </RepositoryInfo>
+
+      ) : (<p>Loading</p>)}
+
+      <Issues>
+        {issues.map(issue => (
+          <a key={issue.id} href={issue.html_url}>
+            <div>
+              <strong>{issue.title}</strong>
+              <p>{issue.user.login}</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+      </Issues>
+
+    </>
+  );
 };
 
 export default Repository;
