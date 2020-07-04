9c3509e510f1933ed5ed4f0b74d60d9b30774225
gabrielrsp <gabrielrsp18@gmail.com>
2020-07-04 00:57:41 -0300

    save repos on localstorage

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,12 +1,20 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { shade } from 'polished';
-export const Title = styled.h1`
-font-size: 48px;
-color: #3a3a3a;
 
+interface FormProps {
+  hasError: boolean;
+}
+
+export const Title = styled.h1`
+  font-size: 48px;
+  color: #3a3a3a;
+  max-width: 450px;
+  line-height: 56px;
+
+  margin-top: 80px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
 
@@ -19,6 +27,12 @@ export const Form = styled.form`
     border: 0;
     border-radius: 5px 0 0 5px;
     color: #3a3a3a;
+    border: 2px solid #fff;
+    border-right: 0;
+
+    ${(props) => props.hasError && css`
+      border-color: #c53030;
+    ` }
 
     &::placeholder {
       color: #a8a8b3;
@@ -37,12 +51,15 @@ export const Form = styled.form`
 
     &:hover {
       background: ${shade(0.2, '#04d361')};
-
-
     }
-
   }
 
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
 `;
 
 export const Repositories = styled.div`
